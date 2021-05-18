dc9fc535136ea8c7f01a8e09d8957dc696e5d784
DiopAmary <amarydiop27@gmail.com>
2021-05-18 22:56:28 +0100

    Reducer class implementation

--- src/main/java/WordCountReducer.java
+++ src/main/java/WordCountReducer.java
@@ -1,2 +1,17 @@
-public class WordCountReducer {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordCountReducer extends Reducer<Text, LongWritable, Text, LongWritable>{
+
+    @Override
+    protected void reduce(Text key, Iterable<LongWritable> listeValue, Reducer<Text, LongWritable, Text, LongWritable>.Context context) throws IOException, InterruptedException{
+        long count = 0;
+        for (LongWritable v : listeValue){
+            count += v.get();
+        }
+        context.write(key, new LongWritable(count));
+    }
 }
