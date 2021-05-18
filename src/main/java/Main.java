808f25e4c6ce077182c93c634453f6d0a43e8c3b
DiopAmary <amarydiop27@gmail.com>
2021-05-18 23:27:56 +0100

    Driver implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] arg){
-        System.out.println("Wordcount ==========> Hello world in hadoop architecture");
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration, "WordCount");
+        job.setOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setJarByClass(Main.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
